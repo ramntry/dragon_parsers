8e6d5d9f895ed6bd20c352bd1f1da69259d43cb3
ramntry <ramntry@gmail.com>
2013-01-05 05:32:46 +0400

    FIRST sets building is added

--- slr_parser_generator/Grammar.cpp
+++ slr_parser_generator/Grammar.cpp
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <iostream>
 #include <typeinfo>
 #include <set>
@@ -5,6 +6,7 @@
 
 Grammar::Grammar(const Nonterminal &startNonterminal)
     : mStart(&startNonterminal)
+    , mInited(false)
 {
 }
 
@@ -16,10 +18,10 @@ void Grammar::printFormsForNonterminal(const Nonterminal *nonterminal) const
     std::vector<const Nonterminal *> toPrint;
 
     std::size_t numofForms= nonterminal->sententialForms().size();
-    for (std::size_t r = 0; r < numofForms; ++r) {
-        std::cout << "Form No (" << absoluteCounter++ << ", " << r << ") for n" << nonterminal->id() << ":";
-        Symbol::FormIterator formEnd = nonterminal->formEndAt(r);
-        for (Symbol::FormIterator formIt = nonterminal->formBeginAt(r); formIt != formEnd; ++formIt) {
+    for (std::size_t i = 0; i < numofForms; ++i) {
+        std::cout << "Form No (" << absoluteCounter++ << ", " << i << ") for n" << nonterminal->id() << ":";
+        Symbol::FormIterator formEnd = nonterminal->formEndAt(i);
+        for (Symbol::FormIterator formIt = nonterminal->formBeginAt(i); formIt != formEnd; ++formIt) {
             const Nonterminal *nonterminal = (**formIt).getAsNonterminal();
             if (nonterminal) {
                 std::cout << " n";
@@ -43,9 +45,74 @@ void Grammar::printFormsForNonterminal(const Nonterminal *nonterminal) const
     absoluteCounter = 0;
 }
 
+void Grammar::printAllFirstSets()
+{
+    init();
+    IdToSet::const_iterator setEnd = mFirstSets.end();
+    for (IdToSet::const_iterator setIt = mFirstSets.begin(); setIt != setEnd; ++setIt) {
+        std::cout << "First set for n" << setIt->first << ":";
+        IdSet::const_iterator end = setIt->second.end();
+        for (IdSet::const_iterator it = setIt->second.begin(); it != end; ++it) {
+            std::cout << " t" << *it;
+        }
+        std::cout << "\n";
+    }
+}
+
+void Grammar::init()
+{
+    if (mInited) {
+        return;
+    }
+
+    makeFirstSets();
+    makeFollowSets();
+
+    mInited = true;
+}
+
+void Grammar::makeFirstSets()
+{
+    makeFirstSetFor(mStart);
+}
+
+const Grammar::IdSet &Grammar::makeFirstSetFor(const Nonterminal *nonterminal)
+{
+    IdSet &target = mFirstSets[nonterminal->id()];
+    if (!target.empty()) {
+        return target;
+    }
+    std::size_t numofForms = nonterminal->sententialForms().size();
+    for (std::size_t i = 0; i < numofForms; ++i) {
+        const Symbol *firstSymbol = *nonterminal->formBeginAt(i);
+        if (firstSymbol->getAsTerminal()) {
+            target.insert(firstSymbol->id());
+        } else {
+            const IdSet &resultForFirstSymbol = makeFirstSetFor(firstSymbol->getAsNonterminal());
+            target.insert(resultForFirstSymbol.begin(), resultForFirstSymbol.end());
+        }
+    }
+    return target;
+}
+
+void Grammar::makeFollowSets()
+{
+}
+
+bool Grammar::inFirstSet(int terminalId, int nonterminalId)
+{
+    init();
+    IdToSet::const_iterator firstSetIt = mFirstSets.find(nonterminalId);
+    assert(firstSetIt != mFirstSets.end());
+    return firstSetIt->second.find(terminalId) != firstSetIt->second.end();
+}
+
 void Grammar::test()
 {
     std::cout << "Test grammar with typeid.name " << typeid(*this).name() << "\n";
     printFormsForNonterminal(mStart);
+
+    std::cout << "First sets:\n";
+    printAllFirstSets();
     std::cout << std::endl;
 }
--- slr_parser_generator/Grammar.h
+++ slr_parser_generator/Grammar.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <set>
+#include <map>
 #include "Nonterminal.h"
 
 class Grammar
@@ -7,9 +9,24 @@ public:
     Grammar(const Nonterminal &startNonterminal);
     virtual ~Grammar() {}
 
+    bool inFirstSet(int terminalId, int nonterminalId);
+
     void test();
     void printFormsForNonterminal(const Nonterminal *nonterminal) const;
+    void printAllFirstSets();
 
-public:
+private:
+    typedef std::set<int> IdSet;
+    typedef std::map<int, IdSet> IdToSet;
+
+    void init();
+    void makeFirstSets();
+    void makeFollowSets();
+    const Grammar::IdSet &makeFirstSetFor(const Nonterminal *nonterminal);
+
     const Nonterminal *mStart;
+
+    bool mInited;
+    IdToSet mFirstSets;
+    IdToSet mFollowSets;
 };
